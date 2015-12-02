ab1ee7ca5397f67391a865749121e212704367cd
Kyle Mellander <kylemellander@gmail.com>
2015-12-02 12:18:52 -0800

    Day 2 challenge 1 complete

---
+++ day2.js
@@ -0,0 +1,58 @@
+var fs = require('fs');
+
+function surface(l, h, w) {
+  return 2*l*w + 2*w*h + 2*h*l;
+}
+
+function slack(l, h, w) {
+  return Math.min(l*w, l*h, w*h);
+}
+
+function sqFtForPackage(l, h, w) {
+  return surface(l, h, w) + slack(l, h, w);
+}
+
+function readPackages(file) {
+  fs.readFile(file, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var rawDataArray = data.split("\n");
+
+    var packages = rawDataArray.map(function(package) {
+      return package.split("x");
+    });
+
+    packages.pop();
+
+    console.log(packages);
+    return packages;
+  });
+}
+
+function calculateSqFtForAllPackages(file) {
+  var packages;
+  var result = 0;
+  fs.readFile(file, 'utf8', function(err, data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var rawDataArray = data.split("\n");
+
+    packages = rawDataArray.map(function(package) {
+      return package.split("x");
+    });
+
+    packages.pop();
+
+    packages.forEach(function(package) {
+      result += sqFtForPackage(package[0], package[1], package[2]);
+    });
+
+    console.log(result);
+  });
+}
+
+calculateSqFtForAllPackages('packages.txt');
