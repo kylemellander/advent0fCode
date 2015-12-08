041b04aa95cf006947000d99958f43b8ee8f23e9
Kyle Mellander <kylemellander@gmail.com>
2015-12-07 21:29:00 -0800

    Santa talks elvish to me

--- day6/day6.js
+++ day6/day6.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-function turnTheLightsOn(file) {
+function turnTheLightsOn(file, lang) {
   fs.readFile(file, 'utf8', function(err, data) {
     var lights = {};
 
@@ -20,14 +20,14 @@ function turnTheLightsOn(file) {
     });
 
     instructions.forEach(function(instruction) {
-      instruction == [] ? lights = lights : lights = messWithTheLights(lights, instruction);
+      lights = messWithTheLights(lights, instruction, lang);
     });
 
     console.log(lightsOn(lights));
   });
 }
 
-function messWithTheLights(lights, instruction) {
+function messWithTheLights(lights, instruction, lang) {
   var firstX = parseInt(instruction[1]);
   var firstY = parseInt(instruction[2]);
   var lastX = parseInt(instruction[3]);
@@ -36,19 +36,39 @@ function messWithTheLights(lights, instruction) {
   if (instruction[0] === "on") {
     for (var i = firstX; i <= lastX; ++i) {
       for (var j = firstY; j <= lastY; ++j) {
-        lights[i+','+j] = true;
+        if (lang === "elvish" && lights[i+','+j]) {
+          lights[i+','+j] += 1;
+        } else {
+          lights[i+','+j] = 1;
+        }
       }
     }
   } else if (instruction[0] === "off") {
     for (var i = firstX; i <= lastX; ++i) {
       for (var j = firstY; j <= lastY; ++j) {
-        lights[i+','+j] = false;
+        if (lang === "elvish" && lights[i+','+j] && lights[i+','+j] >= 0) {
+            lights[i+','+j] -= 1;
+        } else {
+          lights[i+','+j] = 0;
+        }
       }
     }
   } else {
     for (var i = firstX; i <= lastX; ++i) {
       for (var j = firstY; j <= lastY; ++j) {
-        lights[i+','+j] = !lights[i+','+j];
+        if (lang === "elvish") {
+          if (lights[i+','+j]) {
+            lights[i+','+j] += 2;
+          } else {
+            lights[i+','+j] = 2;
+          }
+        } else {
+          if (lights[i+','+j]) {
+            lights[i+','+j] = 1 - lights[i+','+j];
+          } else {
+            lights[i+','+j] = 1;
+          }
+        }
       }
     }
   }
@@ -59,9 +79,12 @@ function messWithTheLights(lights, instruction) {
 function lightsOn(obj) {
   var size = 0, key;
   for (key in obj) {
-    if (obj.hasOwnProperty(key) && obj[key]) size++;
+    if (obj.hasOwnProperty(key)) {
+      size += obj[key];
+    }
   }
   return size;
 }
 
-turnTheLightsOn('directions.txt');
+turnTheLightsOn('directions.txt', 'english');
+turnTheLightsOn('directions.txt', 'elvish');
