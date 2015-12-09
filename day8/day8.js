1fb0771d256bcdbffdc718668bd15736b585e76d
Kyle Mellander <kylemellander@gmail.com>
2015-12-08 21:28:22 -0800

    It encodes too

--- day8/day8.js
+++ day8/day8.js
@@ -8,6 +8,10 @@ function countStringDisplay(c) {
   return c.replace(/\\\\|\\"|\\x[a-f0-9]{2}/g, "a").length - 2;
 }
 
+function countStringEncode(c) {
+  return c.replace(/\\|\"/g, "aa").length + 2;
+}
+
 function spaceSaved(file) {
   var data = fs.readFileSync(file, 'utf-8').trimRight().split('\n');
   var result = 0;
@@ -20,4 +24,17 @@ function spaceSaved(file) {
   return result;
 }
 
+function spaceGained(file) {
+  var data = fs.readFileSync(file, 'utf-8').trimRight().split('\n');
+  var result = 0;
+
+  data.forEach(function(line) {
+    result -= countStringCode(line);
+    result += countStringEncode(line);
+  });
+
+  return result;
+}
+
 console.log("Space Saved: " + spaceSaved('sleigh.txt'));
+console.log("Space Gained: " + spaceGained('sleigh.txt'));
