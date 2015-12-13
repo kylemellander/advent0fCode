1d3de0bdeca30d380dd8908278a9549d0d202e08
Kyle Mellander <kylemellander@gmail.com>
2015-12-12 18:08:33 -0800

    Refactor to rid of searchArrayForNumbers function

--- day12/day12.js
+++ day12/day12.js
@@ -1,6 +1,8 @@
 var jsonData = require('./input.json');
 
-// searchJsonForNumbers takes 1 required json object and an optional check (for part2) to omit any objects that have a value of the included string.
+/*  searchJsonForNumbers takes 1 required json object and
+    an optional check (for part2) to omit any object that
+    have a value of the included string. */
 
 console.log(searchJsonForNumbers(jsonData));
 console.log(searchJsonForNumbers(jsonData, "red"));
@@ -11,10 +13,8 @@ function searchJsonForNumbers(json, check) {
   if (!check || untainted(json, check)) {
     for (var key in json) {
       if (json.hasOwnProperty(key)) {
-        if (json[key].constructor === Object) {
+        if (typeof json[key] === "object") {
           count += searchJsonForNumbers(json[key], check);
-        } else if (json[key].constructor === Array) {
-          count += searchArrayForNumbers(json[key], check);
         } else if (typeof json[key] === "number") {
           count += json[key]
         }
@@ -25,26 +25,12 @@ function searchJsonForNumbers(json, check) {
   return count;
 }
 
-function searchArrayForNumbers(array, check) {
-  var count = 0;
-
-  array.forEach(function(item) {
-    if (item.constructor === Array) {
-      count += searchArrayForNumbers(item, check);
-    } else if (item.constructor === Object) {
-      count += searchJsonForNumbers(item, check);
-    } else if (typeof item === "number") {
-      count += item;
-    }
-  });
-
-  return count;
-}
-
 function untainted(json, check) {
-  for (key in json) {
-    if (json[key] === check) {
-      return false;
+  if (json.constructor !== Array) {
+    for (key in json) {
+      if (json[key] === check) {
+        return false;
+      }
     }
   }
 
