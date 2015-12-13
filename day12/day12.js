aeda0ccf2473fcac9d2d03be95948d7fd8da4fa1
Kyle Mellander <kylemellander@gmail.com>
2015-12-12 17:10:05 -0800

    Got rid of those pesky red objects

--- day12/day12.js
+++ day12/day12.js
@@ -1,42 +1,54 @@
 var jsonData = require('./input.json');
 
 console.log(searchJsonForNumbers(jsonData));
+console.log(searchJsonForNumbers(jsonData, 0, true));
 
-function searchJsonForNumbers(json, count) {
+function searchJsonForNumbers(json, count, untaintedCheck) {
   if (!count) count = 0;
 
-  for (var key in json) {
-    if (json.hasOwnProperty(key)) {
-      if (json[key].constructor === Object) {
-        var result = searchJsonForNumbers(json[key], count);
-        count = result;
-      } else if (json[key].constructor === Array) {
-        count += searchArrayForNumbers(json[key]);
-      } else if (typeof json[key] === "number") {
-        count += json[key]
+  if (!untaintedCheck || untainted(json)) {
+    for (var key in json) {
+      if (json.hasOwnProperty(key)) {
+        if (json[key].constructor === Object) {
+          var result = searchJsonForNumbers(json[key], count, untaintedCheck);
+
+          count = result;
+        } else if (json[key].constructor === Array) {
+          count += searchArrayForNumbers(json[key], untaintedCheck);
+        } else if (typeof json[key] === "number") {
+          count += json[key]
+        }
       }
     }
   }
+
   return count;
 }
 
-function searchArrayForNumbers(array) {
+function searchArrayForNumbers(array, untaintedCheck) {
   var count = 0;
 
   array.forEach(function(item) {
     if (item.constructor === Array) {
-      count += searchArrayForNumbers(item);
+      count += searchArrayForNumbers(item, untaintedCheck);
     } else if (item.constructor === Object) {
-      var deeperJson = {
-        count: count,
-        jsonData: item
-      }
+      var result = searchJsonForNumbers(item, count, untaintedCheck);
 
-      count = searchJsonForNumbers(deeperJson);
+      count = result;
     } else if (typeof item === "number") {
       count += item;
     }
   });
 
   return count;
+}
+
+function untainted(json) {
+  for (key in json) {
+    if (json[key] === "red") {
+      return false;
+    }
+  }
+
+  return true;
 }
