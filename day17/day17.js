7fdfcff8f95a8036051a405da9c7fb98158cea25
Kyle Mellander <kylemellander@gmail.com>
2015-12-17 14:46:15 -0800

    Eggnog is tucked away in the fridge

---
+++ day17/day17.js
@@ -0,0 +1,33 @@
+var fs = require('fs');
+
+function eggNogCombos(file, amount, minimum) {
+  var data = fs.readFileSync(file, 'utf-8').trimRight().split('\n').map(function(x) {return parseInt(x);});
+  var binary = "";
+  var bottles = [];
+  var smallestBottleCount;
+
+  for (var i = 0; i < data.length; i++) {
+    binary += "1";
+  }
+
+  var combos = parseInt(binary, 2);
+
+  for (var i = 0; i <= combos; i++) {
+    var filled = 0;
+    var bottleCount = 0;
+    var arr = i.toString(2).split("").reverse();
+
+    arr.forEach(function(val, index) {
+      if (val === "1") bottleCount += 1;
+      filled += data[index] * val;
+    });
+
+    if (filled === amount) bottles.push(bottleCount);
+    if (filled === amount && (!smallestBottleCount || smallestBottleCount > bottleCount)) smallestBottleCount = bottleCount;
+  }
+  
+  return minimum ? bottles.filter(function(x) {return x === smallestBottleCount;}).length : bottles.length;
+}
+
+console.log(eggNogCombos('input.txt', 150));
+console.log(eggNogCombos('input.txt', 150, true));
