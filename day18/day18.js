bcbc85173d5335c0780450ad24cdd71967750b00
Kyle Mellander <kylemellander@gmail.com>
2015-12-17 22:11:57 -0800

    Refactor

--- day18/day18.js
+++ day18/day18.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 
 function christmasLights(file, iterations, stuckOn) {
-  var data = fs.readFileSync(file, 'utf-8').trimRight().split('\n').map(function(x) {return x.split("");});
+  var data = fs.readFileSync(file, 'utf-8').trimRight().replace(/\n/g, "").split("");
 
   for (var i = 1; i <= iterations; i++) {
     data = adjustLights(data, stuckOn);
@@ -11,33 +11,33 @@ function christmasLights(file, iterations, stuckOn) {
 }
 
 function countLights(data) {
-  return data.map(function(x) {return x.join("");}).join("").replace(/\./g, "").length;
+  return data.filter(function(x) {return x === "#";}).length;
 }
 
 function adjustLights(data, stuckOn) {
   var newData = [];
 
   for (var x = 0; x < 100; x++) {
-    var newXData = [];
     for (var y = 0; y < 100; y++) {
       var neighborsOn = 0;
       var status;
+      var i = x + y * 100;
 
       if (stuckOn) {
-        if ((data[0][0] === "." && x <= 1 && y <= 1) && !(x === 0 && y === 0)) neighborsOn += 1;
-        if ((data[0][99] === "." && x <= 1 && y > 97) && !(x === 0 && y === 99)) neighborsOn += 1;
-        if ((data[99][0] === "." && x > 97 && y <= 1) && !(x === 99 && y === 0)) neighborsOn += 1;
-        if ((data[99][99] === "." && x > 97 && y > 97) !(x === 99 && y === 99)) neighborsOn += 1;
+        if ((data[0] === "." && x <= 1 && y <= 1) && !(x === 0 && y === 0)) neighborsOn += 1;
+        if ((data[99] === "." && x <= 1 && y > 97) && !(x === 0 && y === 99)) neighborsOn += 1;
+        if ((data[9900] === "." && x > 97 && y <= 1) && !(x === 99 && y === 0)) neighborsOn += 1;
+        if ((data[9999] === "." && x > 97 && y > 97) && !(x === 99 && y === 99)) neighborsOn += 1;
       }
-      if (x > 0 && y > 0 && data[x-1][y-1] === "#") neighborsOn += 1;
-      if (y > 0 && data[x][y-1] === "#") neighborsOn += 1;
-      if (x < 99 && y > 0 && data[x+1][y-1] === "#") neighborsOn += 1;
-      if (x > 0 && data[x-1][y] === "#") neighborsOn += 1;
-      if (x < 99 && data[x+1][y] === "#") neighborsOn += 1;
-      if (x > 0 && y < 99 && data[x-1][y+1] === "#") neighborsOn += 1;
-      if (y < 99 && data[x][y+1] === "#") neighborsOn += 1;
-      if (x < 99 && y < 99 && data[x+1][y+1] === "#") neighborsOn += 1;
-      if (data[x][y] === "#") {
+      if (x > 0 && y > 0 && data[i-101] === "#") neighborsOn += 1;
+      if (y > 0 && data[i-100] === "#") neighborsOn += 1;
+      if (x < 99 && y > 0 && data[i-99] === "#") neighborsOn += 1;
+      if (x > 0 && data[i-1] === "#") neighborsOn += 1;
+      if (x < 99 && data[i+1] === "#") neighborsOn += 1;
+      if (x > 0 && y < 99 && data[i+99] === "#") neighborsOn += 1;
+      if (y < 99 && data[i+100] === "#") neighborsOn += 1;
+      if (x < 99 && y < 99 && data[i+101] === "#") neighborsOn += 1;
+      if (data[i] === "#") {
         if (stuckOn && ((x === 0 && y === 0) || ((x === 0 && y === 99)) || (x === 99 && y === 0) || (x === 99 && y === 99))) {
           status = "#";
         } else if (neighborsOn === 2 || neighborsOn ===3) {
@@ -52,10 +52,8 @@ function adjustLights(data, stuckOn) {
           status = ".";
         }
       }
-
-      newXData.push(status);
+      newData.push(status);
     }
-    newData.push(newXData);
   }
 
   return newData;
